697d6294114ab1927eb155cadc59e12811450b0f
Ritika Das <61988162+Ritika-Das@users.noreply.github.com>
2020-10-01 13:51:33 +0530

    Create Coloured Square Wave Pulse.py

---
+++ Coloured Square Wave Pulse.py
@@ -0,0 +1,14 @@
+import turtle
+
+t = turtle.Turtle()
+
+for c in ['red', 'green', 'blue', 'purple']:
+    t.color(c)
+    t.forward(70)
+    t.left(90)
+    t.forward(70)
+    t.right(90)
+    t.forward(70)
+    t.right(90)
+    t.forward(70)
+    t.left(90)
